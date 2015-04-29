23cd630795e13df08611de7df6ab697050308ffb
ColorFuzzy <color.fuzzy@gmail.com>
2015-04-29 11:17:24 +0800

    fix: nginx install

--- stack/scripts/_start_db.sh
+++ stack/scripts/_start_db.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
-
-# PostgreSQL for Django/DeliveryService
+#--------------------------------------------------------------
+# PostgreSQL相关文件的权限设置(多服务共用, 使用不同的库)
 chown -R postgres "$PGDATA"
 chmod g+s /run/postgresql
 chown -R postgres:postgres /run/postgresql
+
+# PostgreSQL配置文件
+
+# PostgreSQL启动方法, 暂停直接kill
+
+#--------------------------------------------------------------
+# Mongo基本设置(多服务共用, 使用不同的库)
+chown -R mongodb /db/mongodb
+
+# Mongo配置文件
+
+# Mongo启动方法, 暂停直接kill

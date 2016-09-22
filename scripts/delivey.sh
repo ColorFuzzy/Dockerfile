1552de2a91d1cf1b8e60769efd52964221427604
Color Fuzzy <color.fuzzy@gmail.com>
2016-09-22 19:26:07 +0800

    remove waimaichaoren file

--- scripts/delivey.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH=/venv/DeliveryService/bin:$PATH
-
-cp /deploy/conf/delivery.app.conf /venv/DeliveryService/src/github.com/WaimaiChaoren/DeliveryService/conf/app.conf
-cd /venv/DeliveryService/src/github.com/WaimaiChaoren/DeliveryService
-go build main.go
-nohup ./main &
